e499a4a2f47d3956e0cf1c9b8934cfd673b34f8a
Edgar Vasquez <edgar@vsqz.dev>
2023-12-24 10:14:44 -0600

    Fix day 1 -- account for repeated spelled out digits

--- day01/Program.cs
+++ day01/Program.cs
@@ -1,67 +1,83 @@
-﻿static void Part1()
+﻿static class Day01
 {
-    CalculateCalibrationValues(false);
-}
-
-static void Part2()
-{
-    CalculateCalibrationValues(true);
-}
-
-static void CalculateCalibrationValues(bool digitize)
-{
-    var sum = 0;
-
-    foreach (var line in File.ReadLines(@"./input.txt"))
+    static void Part1()
     {
-        var lineToProcess = digitize ? DigitizeString(line) : line;
-
-        var digits = lineToProcess.Where(c => char.IsDigit(c));
-        var number = int.Parse($"{digits.First()}{digits.Last()}");
-        sum += number;
+        CalculateCalibrationValues(false);
     }
 
-    Console.WriteLine(sum);
-}
-
-static string DigitizeString(string input)
-{
-    var digitMap = new Dictionary<string, string> {
-        {"one", "1"},
-        {"two", "2"},
-        {"three", "3"},
-        {"four", "4"},
-        {"five", "5"},
-        {"six", "6"},
-        {"seven", "7"},
-        {"eight", "8"},
-        {"nine", "9"}
-    };
-
-    var orderedLocations = new SortedDictionary<int, string>();
-    foreach (var digit in digitMap)
+    static void Part2()
     {
-        if (input.Contains(digit.Key))
-            orderedLocations.Add(input.IndexOf(digit.Key), digit.Key);
+        CalculateCalibrationValues(true);
     }
 
-    for (int i = 0; i < input.Length; ++i)
+    static void CalculateCalibrationValues(bool digitize)
     {
-        if (char.IsDigit(input[i]))
-            orderedLocations.Add(i, $"{input[i]}");
+        var sum = 0;
+
+        foreach (var line in File.ReadLines(@"./input.txt"))
+        {
+            var digits = digitize ? GetDigitCharsWithWords(line) : GetDigitChars(line);
+            var number = int.Parse($"{digits.First()}{digits.Last()}");
+            sum += number;
+        }
+
+        Console.WriteLine(sum);
     }
 
-    var firstReplacement = orderedLocations.FirstOrDefault();
-    var lastReplacement = orderedLocations.LastOrDefault();
+    static IEnumerable<char> GetDigitChars(string input)
+    {
+        return input.Where(c => char.IsDigit(c));
+    }
 
-    if (digitMap.ContainsKey(firstReplacement.Value))
-        input = input.Replace(firstReplacement.Value, digitMap[firstReplacement.Value]);
+    static IEnumerable<char> GetDigitCharsWithWords(string input)
+    {
+        var digitMap = new Dictionary<string, char> {
+            {"one", '1'},
+            {"two", '2'},
+            {"three", '3'},
+            {"four", '4'},
+            {"five", '5'},
+            {"six", '6'},
+            {"seven", '7'},
+            {"eight", '8'},
+            {"nine", '9'}
+        };
 
-    if (digitMap.ContainsKey(lastReplacement.Value))
-        input = input.Replace(lastReplacement.Value, digitMap[lastReplacement.Value]);
+        var orderedLocations = new SortedDictionary<int, char>();
+        foreach (var digit in digitMap)
+        {
+            if (input.Contains(digit.Key))
+            {
+                var indices = input.IndicesOf(digit.Key);
+                indices.ToList().ForEach(i => orderedLocations.Add(i, digit.Value));                
+            }
+        }
 
-    return input;
+        for (int i = 0; i < input.Length; ++i)
+        {
+            if (char.IsDigit(input[i]))
+                orderedLocations.Add(i, input[i]);
+        }
+
+        var firstReplacement = orderedLocations.FirstOrDefault();
+        var lastReplacement = orderedLocations.LastOrDefault();
+
+        return new char[] {firstReplacement.Value, lastReplacement.Value};
+    }
+
+    static IEnumerable<int> IndicesOf(this string target, string search)
+    {
+        int location = target.IndexOf(search);
+        while (location != -1)
+        {
+            yield return location;
+            location = target.IndexOf(search, location + 1);
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Part1();
+        Part2();
+    }
 }
-
-Part1();
-Part2();
